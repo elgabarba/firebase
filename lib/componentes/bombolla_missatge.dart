d27baa6560679bba6961499d990f00715654002b
elgabarba <elgabarba@ceroca.cat>
2025-03-12 16:34:47 +0100

    Desplaçament llista missatges

--- lib/componentes/bombolla_missatge.dart
+++ lib/componentes/bombolla_missatge.dart
@@ -1,16 +1,31 @@
+import 'package:firebase/auth/servei_auth.dart';
 import 'package:flutter/material.dart';
 
 class BombollaMissatge extends StatelessWidget {
   final String missatge;
-  const BombollaMissatge({super.key, required this.missatge});
+  final String idAutor;
+  const BombollaMissatge(
+      {super.key, required this.missatge, required this.idAutor});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 5, right: 5, top: 10),
-      child: Container(
-        color: Colors.amber[200],
-        child: Text(missatge),
+      child: Align(
+        alignment: idAutor == ServeiAuth().getUsuariActual()!.uid
+            ? Alignment.centerRight
+            : Alignment.centerLeft,
+        child: Container(
+          decoration: BoxDecoration(
+              color: idAutor == ServeiAuth().getUsuariActual()!.uid
+                  ? Colors.green[200]
+                  : Colors.amber[200],
+              borderRadius: BorderRadius.circular(10)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(missatge),
+          ),
+        ),
       ),
     );
   }
--- lib/paginas/pagChat.dart
+++ lib/paginas/pagChat.dart
@@ -5,19 +5,32 @@ import 'package:firebase/componentes/bombolla_missatge.dart';
 import 'package:flutter/material.dart';
 
 class Pagchat extends StatefulWidget {
-
   final String idReceptor;
 
   const Pagchat({super.key, required this.idReceptor});
-
 
   @override
   State<Pagchat> createState() => _PaginaChatState();
 }
 
 class _PaginaChatState extends State<Pagchat> {
-
   final TextEditingController tecMissatge = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), (){
+      ferScrollCapAvall();
+    });
+  }
+
+  void ferScrollCapAvall(){
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent + 100,
+        duration: const Duration(seconds: 1), 
+        curve: Curves.fastOutSlowIn);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,47 +50,56 @@ class _PaginaChatState extends State<Pagchat> {
       ),
     );
   }
-  
+
   Widget _crearZonaMostrarMissatges() {
     return Expanded(
-      child: StreamBuilder(
-        stream: ServeiChat().getMissatges(ServeiAuth().getUsuariActual()!.uid, widget.idReceptor), 
-        builder: (context, snapshot){
-          if (snapshot.hasError) {
-            return const Text("Error caregant datos...");
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text("Carregant missatges...");
-          }
-          return ListView(
-            children: snapshot.data!.docs.map((document) => _construirItemMissatge(document)).toList(),
-          );
-        }
-      )
+        child: StreamBuilder(
+            stream: ServeiChat().getMissatges(
+                ServeiAuth().getUsuariActual()!.uid, widget.idReceptor),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Text("Error caregant datos...");
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("Carregant missatges...");
+              }
+              return ListView(
+                controller: _scrollController,
+                children: snapshot.data!.docs
+                    .map((document) => _construirItemMissatge(document))
+                    .toList(),
+              );
+            }));
+  }
+
+  Widget _construirItemMissatge(DocumentSnapshot documentSnapshot) {
+    Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
+    return BombollaMissatge(
+      missatge: data["missatge"],
+      idAutor: data["idAutor"],
     );
   }
 
-  Widget _construirItemMissatge(DocumentSnapshot documentSnapshot){
-    Map<String, dynamic> data = documentSnapshot.data() as Map<String, dynamic>;
-    return BombollaMissatge(missatge: data["missatge"],);
-  }
-  
   Widget _crearZonaEscriureMissatge() {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: Row(
         children: [
-          Expanded(child: TextField( 
-            controller:  tecMissatge,
-            decoration: InputDecoration(
-              filled: true, 
-              fillColor: Colors.amber[200]
-            ),
+          Expanded(
+              child: TextField(
+            controller: tecMissatge,
+            decoration:
+                InputDecoration(filled: true, fillColor: Colors.amber[200]),
           )),
-          const SizedBox(width: 10,),
+          const SizedBox(
+            width: 10,
+          ),
           IconButton(
-            onPressed: enviarMissatge, 
-            icon: const Icon(Icons.send, color: Colors.white,),
+            onPressed: enviarMissatge,
+            icon: const Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
             style: const ButtonStyle(
               backgroundColor: WidgetStatePropertyAll(Colors.green),
             ),
@@ -88,9 +110,13 @@ class _PaginaChatState extends State<Pagchat> {
   }
 
   void enviarMissatge() {
-    if (tecMissatge.text.isNotEmpty){
+    if (tecMissatge.text.isNotEmpty) {
       ServeiChat().enviarMissatge(widget.idReceptor, tecMissatge.text);
+      tecMissatge.clear();
+      Future.delayed(const Duration(milliseconds: 250), (){
+        ferScrollCapAvall();
+      });
+      
     }
-
   }
 }
